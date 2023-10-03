44178466ebc656bdabf3fd7bee15e2154fe7ccfb
youngbfg <bilalfurkangenc@gmail.com>
2023-10-03 16:23:21 +0300

    d

--- pages/home/homes.jsx
+++ pages/home/homes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Video from "../../components/video/Video";
 import Home_Page_Navbar from "../../components/homepagenavbar/HomePageNavbar";
+import Counter from "../../components/counter/Counter";
 
 const Index = () => {
   return (
     <React.Fragment>
       <Video />
       <Home_Page_Navbar />
+      <Counter/>
     </React.Fragment>
   );
 };
