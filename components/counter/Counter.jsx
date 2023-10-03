b20cd63cdd5d0a064de59b1634045d28c4cb8a07
youngbfg <bilalfurkangenc@gmail.com>
2023-10-03 16:23:35 +0300

    ddf

---
+++ components/counter/Counter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./counter.module.css";
+
+
+const Counter = () => {
+  return (
+    <div className={styles.body}>
+      <div className={styles.wrapper}>
+        <div className={styles.counter}>
+          <div className={styles.col_fourth}>
+            <div className={styles.end}>
+              <i className={styles.fa_2x}></i>
+              <h2
+                className={styles.count_title}
+                data-to="300"
+                data-speed="1500"
+              ></h2>
+              300
+            </div>
+            <p className={styles.count_text}>SomeText GoesHere</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
