82f85c1768c4cc8a7b845fc8f49a2d9e2f0f7335
youngbfg <bilalfurkangenc@gmail.com>
2023-10-04 16:00:24 +0300

    counter

--- components/counter/Counter.jsx
+++ components/counter/Counter.jsx
@@ -1,27 +1,123 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./counter.module.css";
-
+import CountUp from "react-countup";
+import { BiNews } from "react-icons/bi";
+import { BiSolidNews } from "react-icons/bi";
+import { BsNewspaper } from "react-icons/bs";
+import { FaNewspaper } from "react-icons/fa";
 
 const Counter = () => {
+  var suffix = "";
+  var prefix = "%";
   return (
     <div className={styles.body}>
+      {/* -----------------1.konteyner---------------- */}
       <div className={styles.wrapper}>
         <div className={styles.counter}>
-          <div className={styles.col_fourth}>
-            <div className={styles.end}>
-              <i className={styles.fa_2x}></i>
-              <h2
-              
-                className={styles.count_title}
-                data-to="300"
-                data-speed="1500"
-              ></h2>
-              300
+          {/* -------------resim----------- */}
+          <div className={styles.logo}>
+            <BiNews />
+          </div>
+
+          {/* -----------başlık yazısı---------------- */}
+          <div className={styles.count_title}>
+            <p>Test0</p>
+
+            {/* ----------------rakam------------------------- */}
+            <div className={styles.number}>
+              <CountUp
+                duration={3.35}
+                separator=","
+                start={0.0}
+                end={34000.0}
+                prefix="%"
+              />
             </div>
-            <p className={styles.count_text}>SomeText GoesHere</p>
           </div>
         </div>
       </div>
+      {/* ---------------------------------------------------------------------------- */}
+
+      {/* -----------------2.konteyner---------------- */}
+      <div className={styles.wrapper}>
+        <div className={styles.counter}>
+          {/* -------------resim----------- */}
+          <div className={styles.logo}>
+            <BiSolidNews />
+          </div>
+
+          {/* -----------başlık yazısı---------------- */}
+          <div className={styles.count_title}>
+            <p>Test0</p>
+
+            {/* ----------------rakam------------------------- */}
+            <div className={styles.number}>
+              <CountUp
+                duration={3.35}
+                separator=","
+                start={0.0}
+                end={34000.0}
+                prefix="%"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ---------------------------------------------------------------------------- */}
+
+      {/* -----------------3.konteyner---------------- */}
+      <div className={styles.wrapper}>
+        <div className={styles.counter}>
+          {/* -------------resim----------- */}
+          <div className={styles.logo}>
+            <BsNewspaper />
+          </div>
+
+          {/* -----------başlık yazısı---------------- */}
+          <div className={styles.count_title}>
+            <p>Test0</p>
+
+            {/* ----------------rakam------------------------- */}
+            <div className={styles.number}>
+              <CountUp
+                duration={3.35}
+                separator=","
+                start={0.0}
+                end={34000.0}
+                prefix="%"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ---------------------------------------------------------------------------- */}
+
+      {/* -----------------4.konteyner---------------- */}
+      <div className={styles.wrapper}>
+        <div className={styles.counter}>
+          {/* -------------resim----------- */}
+          <div className={styles.logo}>
+            <FaNewspaper />
+          </div>
+
+          {/* -----------başlık yazısı---------------- */}
+          <div className={styles.count_title}>
+            <p>Test0</p>
+
+            {/* ----------------rakam------------------------- */}
+            <div className={styles.number}>
+              <CountUp
+                duration={3.35}
+                separator=","
+                start={0.0}
+                end={34000.0}
+                prefix="%"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ---------------------------------------------------------------------------- */}
     </div>
   );
 };
